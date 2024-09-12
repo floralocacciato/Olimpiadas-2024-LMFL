aaca750dc139a4cefec373079bbc191c853f81e9
floralocacciato <florencialocacciato@gmail.com>
2024-09-12 09:42:05 -0300

    Creacion de las carpetas navbar, footer y services firestore

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './components/footer/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
