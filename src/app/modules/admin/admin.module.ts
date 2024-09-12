261a477b87d26286ebaa8eb8b36eec294c672bbb
floralocacciato <florencialocacciato@gmail.com>
2024-09-12 10:07:33 -0300

    configuracion de rutas de los modulos admin y administracion

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//archivo de rutas del modulo
 import { AdminRoutingModule } from './admin-routing.module';
+
+//componentes LOCAL
 import { TableComponent } from './components/table/table.component';
+
+//Componente de VISTA
 import { AdminComponent } from './pages/admin/admin.component';
+
+// Componente de MATERIAL
+import { MatIconModule } from '@angular/material/icon';
+
+// Paqueterías de formularios y formularios reactivos de ANGULAR
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,7 +24,17 @@ import { AdminComponent } from './pages/admin/admin.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    TableComponent,
+    AdminComponent,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
--- src/app/modules/autentificacion/autentificacion.module.ts
+++ src/app/modules/autentificacion/autentificacion.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//archivo de rutas del modulo
 import { AutentificacionRoutingModule } from './autentificacion-routing.module';
+
+//componentes del modulo autentificacion 
 import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion/inicio-sesion.component';
 import { RegistroComponent } from './pages/registro/registro/registro.component';
 
+//componentes de material
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+// componentes de angular
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +24,15 @@ import { RegistroComponent } from './pages/registro/registro/registro.component'
   ],
   imports: [
     CommonModule,
-    AutentificacionRoutingModule
+    AutentificacionRoutingModule,
+    //material
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    //angular 
+    FormsModule
   ]
 })
 export class AutentificacionModule { }
