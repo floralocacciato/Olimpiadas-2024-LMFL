aa8f5f2e83b67fe94b4b47897a9849b1832fb06e
floralocacciato <florencialocacciato@gmail.com>
2024-09-12 10:52:00 -0300

    creacion de la carpeta environments, configuracion de firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false, 
+
+    firebaseConfig :{
+  apiKey: "AIzaSyBifGmKAzJC2nBy8nJPMEePCwfBZ6A4AVQ",
+  authDomain: "olimpiadas-2024-24c57.firebaseapp.com",
+  projectId: "olimpiadas-2024-24c57",
+  storageBucket: "olimpiadas-2024-24c57.appspot.com",
+  messagingSenderId: "838248001588",
+  appId: "1:838248001588:web:1f267bd1de2a0c4d541a61"
+}
+
+}
