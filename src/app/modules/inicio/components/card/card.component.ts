c22c46e4c1b52b9bba46c6f0df2275afc6fdd63c
SARAIFIGUEROA <Saraifigueroa012@gmail.com>
2024-09-13 10:40:41 -0300

    creacion de card y carousel

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//importamos la interfaz de producto
+import { Producto } from 'src/app/models/producto';
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -7,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  public info: Producto[];
+  constructor() {
+    this.info = [
+      {
+        idProducto: "",
+        nombre: "",
+        precio: 0,
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        alt:""
+      },
+      {
+        idProducto: "",
+        nombre: "",
+        precio: 0,
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        alt:""
+      },
+      {
+        idProducto: "",
+        nombre: "",
+        precio: 0,
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        alt:""
+      },
+      {
+        idProducto: "",
+        nombre: "",
+        precio: 0,
+        descripcion: "",
+        categoria: "",
+        imagen: "",
+        alt:""
+      },
+    ]
+  }
 }
