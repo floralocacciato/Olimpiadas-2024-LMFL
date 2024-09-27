8300ec1206d245df9693b2c1f3e97f328707643c
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-09-27 10:37:01 -0300

    Declaración de variables que va a utilizar el html de inicio de sesion en el ts,declaración de css y html

--- src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion/inicio-sesion.component.ts
+++ src/app/modules/autentificacion/pages/inicio-sesion/inicio-sesion/inicio-sesion.component.ts
@@ -1,15 +1,121 @@
 import { Component } from '@angular/core';
 
+import { Usuario } from 'src/app/models/usuario';
+
+import { AuthService } from '../../../sevices/auth.service';
+
+import { FirestoreService } from 'src/app/modules/shared/services/firestore.service';
+
+import { Router } from '@angular/router';
+
+import * as CryptoJS from 'crypto-js';
+
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
   styleUrls: ['./inicio-sesion.component.css']
 })
 export class InicioSesionComponent {
+
+
+
+  //Defino la variable hide
+  hide= true
 usuarioIngresado: any;
-hide: any;
-iniciarSesion() {
-throw new Error('Method not implemented.');
+// Constructor que declara las variables provenientes de los componentes AuthService,FirestoreService y Router y las declara como publicas
+      constructor( 
+        public servicioAuth:AuthService,
+      public servicioFirestore:FirestoreService,
+    public servicioRutas: Router 
+  ){}
+       
+      
+
+  //declaro variables que va a usar el usuario
+  usuarios: Usuario  ={
+
+    uid: '', // atributos tipo '' = reciben valores indefinidos,
+    nombre: '',
+    apellido:'',
+    email: '',
+    rol: '',
+    password: ''
+  }
+
+    //CREAR UNA COLECCION QUE SOLO RECIBE OBJETOS DEL TIPO USUARIOS
+    coleccionUsuarios: Usuario[] =[];
+  //creo la funcion de inicio de sesion que va a utilizar 
+
+  //Declaro la funcion y le asigno el tipo async
+
+
+
+  async IniciarSesion(){
+
+      //declaro una constante llamada "credenciales" que viene de la colección de usuarios
+    const credenciales ={
+      email: this.usuarios.email,
+      password: this.usuarios.password
+    }
+
+    try{
+      //Obtenemos el usuario desde la BD -> Cloud FireStore
+      const usuarioBD = await this.servicioAuth.obtenerUsuario(credenciales.email);
+      //! -> si es diferente
+      // empty -> metodo de firebase para marcar algo si s vacio 
+      if(!usuarioBD || usuarioBD.empty){
+
+        alert('Correo electronico no esta registrado')
+      this.LimpiarInputs();
+      return
+      }
+/*Primer documento (registro) en la coleccion de usuarios que se obtiene desde la base de datos
+
+*/
+      const usuarioDoc = usuarioBD.docs[0];
+      /**
+       * Extraer los datos del documento en forma de un objeto y se especifica como de tipo
+       * 'Usuario' -> haciendo referencia a nuestra interfaz de usuario
+       */
+
+      const usuarioData = usuarioDoc.data() as Usuario
+
+      //hash de la contraseña ingresada por el usuario
+      const hashPassword = CryptoJS.SHA256(credenciales.password).toString();
+
+      if(hashPassword !== usuarioData.password){
+        alert('contraseña incorrecta')
+      this.usuarios.password = '';
+      return;
+      }
+
+    const res = await this.servicioAuth.IniciarSesion(credenciales.email, credenciales.password)
+    .then(res=> {
+      alert('Se a logueado con exito');
+      this.servicioRutas.navigate(['/Inicio'])
+    })
+    .catch (err => {
+      alert('Hubo un problema al iniciar sesion '+ err);
+      this.LimpiarInputs();
+
+    })
+    
+    }
+    catch(error){
+      this.LimpiarInputs
+    }
+
+  }
+
+  LimpiarInputs(){
+    const inputs = {
+      uid: this.usuarios.uid = '',
+      nombre: this.usuarios.nombre = '',
+      apellido: this.usuarios.apellido = '',
+      password: this.usuarios.password = '',
+      rol: this.usuarios.rol = '',
+      email: this.usuarios.email=''
+    }
+  }
 }
 
-}
