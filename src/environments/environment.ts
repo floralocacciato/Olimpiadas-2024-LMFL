326e5c56d661a066f7a5dd090a3cbbf194b3055c
zanahoria6 <lopezruiztomas59@gmail.com>
2024-10-24 10:41:11 -0300

    configurar la base de datos nueva y modificamos el inicio sesion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
     production: false, 
 
     firebaseConfig :{
-  apiKey: "AIzaSyBifGmKAzJC2nBy8nJPMEePCwfBZ6A4AVQ",
-  authDomain: "olimpiadas-2024-24c57.firebaseapp.com",
-  projectId: "olimpiadas-2024-24c57",
-  storageBucket: "olimpiadas-2024-24c57.appspot.com",
-  messagingSenderId: "838248001588",
-  appId: "1:838248001588:web:1f267bd1de2a0c4d541a61"
+      apiKey: "AIzaSyDCX9-kmEKk9HtSvvRJ4EH7hRNXpHJc7po",
+      authDomain: "olimpiadas--2024.firebaseapp.com",
+      projectId: "olimpiadas--2024",
+      storageBucket: "olimpiadas--2024.appspot.com",
+      messagingSenderId: "679484067773",
+      appId: "1:679484067773:web:5c75e1be1942edabcd4cd4"
 }
 
 }
