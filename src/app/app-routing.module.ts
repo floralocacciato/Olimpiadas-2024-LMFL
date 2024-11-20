d3b831692bded804b279cb90a35776fda72fd18a
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-11-20 10:56:54 -0300

    un poco mas de css y rueteo a la seccion de carrito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,9 @@ const routes: Routes = [
     path:"",loadChildren:()=>import('./modules/autentificacion/autentificacion.module').then(m=>m.AutentificacionModule)
   },
   {
+    path:"",loadChildren:()=>import('./modules/carrito/carrito.module').then(m=>m.CarritoModule)
+  },
+  {
     
   path:"",loadChildren:()=>import('./modules/admin/admin.module').then(m=>m.AdminModule),
     // Especificamos que la ruta de administrador va a ser protegida con un guardián
