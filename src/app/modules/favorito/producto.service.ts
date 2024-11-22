a965dac2ab23b75ea907ff5207ec3cc38eb3b447
Leandrusk <Leandrosotocarrasco91@gmail.com>
2024-11-22 02:32:39 -0300

    terminada la logica de favoritos para que pueda ser guardado a lo largo del uso de la pagina

--- src/app/modules/favorito/producto.service.ts
+++ src/app/modules/favorito/producto.service.ts
@@ -8,36 +8,55 @@ import { Producto } from 'src/app/models/producto';
   providedIn: 'root'
 })
 export class ProductoService {
- private productos: any[] = []; 
+  private productos: any[] = [];
 
- private favoritosSubject = new BehaviorSubject<Producto[]>([]); // Emisor de favoritos
+  private favoritosSubject = new BehaviorSubject<Producto[]>([]); // Emisor de favoritos
   favoritos$ = this.favoritosSubject.asObservable(); // Observable para componentes
 
-  constructor( ) { }
-// Metodo para contar productos favoritos
-productosFavoritos: any[] = [];
+  constructor() {
+    const favoritosGuardados = localStorage.getItem('favoritos');
+    if (favoritosGuardados) {
+      this.productos = JSON.parse(favoritosGuardados);
+      this.actualizarFavoritos();
+    }
+  }
+  // Metodo para contar productos favoritos
+  productosFavoritos: any[] = [];
 
 
 
 
-setProductos(productos: Producto[]) {
-  this.productos = productos;
-  this.actualizarFavoritos(); // Inicializar lista de favoritos
-}
+  setProductos(productos: Producto[]) {
+    this.productos = productos;
+
+    // Si hay favoritos guardados, actualiza los productos con el estado correspondiente
+    const favoritosGuardados = localStorage.getItem('favoritos');
+    if (favoritosGuardados) {
+      const favoritosIds = JSON.parse(favoritosGuardados).map((p: Producto) => p.idProducto);
+      this.productos.forEach(producto => {
+        producto.favoritos = favoritosIds.includes(producto.idProducto);
+      });
+    }
+
+    this.actualizarFavoritos();
+  }
 
 
 
 
-subirFavorito(producto: Producto) {
-  producto.favoritos = !producto.favoritos; // Cambiar estado de favorito
-  this.actualizarFavoritos(); // Emitir cambios en la lista de favoritos
-}
+  subirFavorito(producto: Producto) {
+    producto.favoritos = !producto.favoritos; // Cambia el estado de favorito
+    this.actualizarFavoritos(); // Actualiza la lista de favoritos
+  }
 
-private actualizarFavoritos() {
-  const favoritos = this.productos.filter(producto => producto.favoritos);
-  this.favoritosSubject.next(favoritos); // Actualizar observable
-}
 
+  private actualizarFavoritos() {
+    const favoritos = this.productos.filter(producto => producto.favoritos);
+    this.favoritosSubject.next(favoritos); // Emite la nueva lista de favoritos
+
+    // Guarda la lista de favoritos en localStorage
+    localStorage.setItem('favoritos', JSON.stringify(favoritos));
+  }
 
 
 }
